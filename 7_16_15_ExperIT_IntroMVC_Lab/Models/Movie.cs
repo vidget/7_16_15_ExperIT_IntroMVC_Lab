30e889db444241af353b27c9d87595718be5134e
Vidget <vidget@yahoo.com>
2015-07-20 16:30:05 -0400

    Part 5 and Part 6
    added Scaffolding Controllers

--- 7_16_15_ExperIT_IntroMVC_Lab/Models/Movie.cs
+++ 7_16_15_ExperIT_IntroMVC_Lab/Models/Movie.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace _7_16_15_ExperIT_IntroMVC_Lab.Models
 {
@@ -10,6 +11,11 @@ namespace _7_16_15_ExperIT_IntroMVC_Lab.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:d}", ApplyFormatInEditMode = true)]
+
         public DateTime ReleaseDate { get; set; }
         public string Genre { get; set; }
         public decimal Price { get; set; }
